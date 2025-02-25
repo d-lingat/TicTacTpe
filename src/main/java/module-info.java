57cfb7062e33d41e7606e5015fe0279621d6ba4b
BinGerry <perezi@gotritons.uog.edu>
2025-02-26 08:07:29 +1000

    added requires javafx.graphics

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.tictactpe {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.graphics;
 
     opens com.example.tictactpe to javafx.fxml;
     exports com.example.tictactpe;
