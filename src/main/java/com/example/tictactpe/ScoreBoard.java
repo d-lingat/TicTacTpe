b4f38ac391a8b5a632a32c63ea98898e42db4664
BinGerry <perezi@gotritons.uog.edu>
2025-03-02 18:15:53 +1000

    Updated ScoreBoard to use Scores

--- src/main/java/com/example/tictactpe/ScoreBoard.java
+++ src/main/java/com/example/tictactpe/ScoreBoard.java
@@ -1,9 +1,9 @@
 package com.example.tictactpe;
 
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.TreeMap;
+
+import com.example.tictactpe.Scores.Score;
 
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
@@ -13,16 +13,10 @@ import javafx.scene.layout.Pane;
 public class ScoreBoard {
     public int max;
     public String scoreTextFormat = "%s: %d";
-    public TreeMap<String, Integer> scoreBoard = new TreeMap<>(); 
     public Pane scoreBoardContainer;
-    public String playerX = "";
-    public String playerO = "";
-    public int playerXScore = 0;
-    public int playerOScore = 0;
-    //public Entry<String, Integer>[] scoreBoard = new Entry[5]; // 0 Index is the highest score while last index is the lowest
+    public Scores scores = new Scores();
 
     public ScoreBoard(int max) {
-        //scoreBoard = new TreeSet<>(Entry.comparingByValue()); 
         this.max = max;
     }
 
@@ -34,99 +28,42 @@ public class ScoreBoard {
      */
     public void updateScoreboardLabels(boolean direction) {
         int idx = 0;
-        Entry<String, Integer> scoreEntry;
-        Iterator<Entry<String, Integer>> scoreEntries;
         ObservableList<Node> ol = scoreBoardContainer.getChildren();
+        ArrayList<Node> al = new ArrayList<>();
         Iterator<Node> labels = ol.iterator();
         Label lbl;
 
-        if (direction) {
-            scoreEntries = scoreBoard.descendingMap().entrySet().iterator();
-        }
-        else {
-            scoreEntries = scoreBoard.entrySet().iterator();
-        }
-        
-        for (Iterator<Entry<String, Integer>> it = scoreEntries; it.hasNext();) {
+        scores.iteratorDescending = direction;
+        scores.resetIndex();
+        System.out.println("index: " + scores.index);
+        for (Score s = scores.next(); scores.hasNext(); s = scores.next()) {
+            System.out.println("index: " + scores.index);
             if (idx > max) {
                 return;
             } 
 
-            scoreEntry = it.next();
             if (labels.hasNext()) {
-
                 lbl = (Label) labels.next();
-                lbl.setText(String.format(scoreTextFormat, scoreEntry.getKey(), scoreEntry.getValue()));
+                lbl.setText(String.format(scoreTextFormat, s.name, s.score));
+                idx++;
                 continue;
             }
 
-            lbl = new Label(String.format(scoreTextFormat, scoreEntry.getKey(), scoreEntry.getValue()));
+            lbl = new Label(String.format(scoreTextFormat, s.name, s.score));
             lbl.prefWidth(100);
-            ol.add(lbl);     
+            lbl.getStyleClass().add("score-lbl");
+            al.add(lbl);     
+            idx++;
         }
-        /*
-        scoreBoard.forEach((score) -> {
-            Label lbl = new Label(String.format(scoreTextFormat, score.getKey(), score.getValue()));
-            lbl.prefWidth(100);
-            ol.add(lbl);     
-        });        
-        */
+
+        ol.addAll(al);
     }
+
+    
 
     protected void updateScoreBoard(final String player) {
-        int score = 100;
-        // this prevents duplicate player names as the tree map allows duplicate values
-        if (scoreBoard.containsKey(player)) {
-            score = scoreBoard.entrySet().stream().filter(s -> 
-                s.getKey().equals(player)
-            ).findAny().map(Map.Entry::getValue).orElse(100);
-            scoreBoard.remove(player, score);
-            score += 100;
-        }
-        scoreBoard.put(player, score);
+        scores.addIfAbsent(player);
     }
-
 
     // Need a way to store information in a file, maybe json
-
-    /* I at first wanted to use a normal array but I figured it is easier to use java.util package 
-    protected void addToScoreBoard(Entry<String, Integer> newScore) {
-        // do nothing if the lowest score in scoreBoard is greater than newScore
-        if (scoreBoard[numOfEntries-1].getValue() > newScore.getValue()) {
-            return;
-        }
-
-        // add to scoreBoard if empty
-        if (numOfEntries == 0) {
-            scoreBoard[numOfEntries] = newScore;
-            numOfEntries++;
-            return;
-        }
-
-        if (numOfEntries < scoreBoard.length) {
-            numOfEntries++;
-        }
-
-        // Algorithm to add to scoreBoard        
-        Entry<String, Integer> nextLowestScore = null; // = Map.entry("error", 999);
-        Entry<String, Integer> tmp;
-        for (int idx = 0; idx < numOfEntries; idx++) {
-            // Continue if newScore is less than scoreBoard[idx] as we don't need to shift values
-            if (scoreBoard[idx].getValue() > newScore.getValue()) {
-                continue;
-            }
-            //  This checks if newScore has been inserted yet, if it has not then it will be inserted 
-            else if (nextLowestScore == null) {
-                    nextLowestScore = scoreBoard[idx];
-                    scoreBoard[idx] = newScore;
-                    continue;
-            }
-
-            //
-            tmp = scoreBoard[idx];
-            scoreBoard[idx] = nextLowestScore;
-            nextLowestScore = tmp;
-        }
-    }
-    */
 }
